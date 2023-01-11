db33e20bf5d5606db86e5fec7575881b8841fb4b
Cory Tepper <corytepper@gmail.com>
2023-01-11 11:26:41 -0500

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Cory Tepper on 1/11/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
